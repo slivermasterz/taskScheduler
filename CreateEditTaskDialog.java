c3d5c4210a44d0f7e2cbb0600cf4b6d54801a3f3
slivermasterz <lokmanslivermaster@gmail.com>
2018-05-04 20:00:36 -0700

    Added Documentation

--- CreateEditTaskDialog.java
+++ CreateEditTaskDialog.java
@@ -1,11 +1,27 @@
 import javax.swing.*;
 import org.jdatepicker.JDatePicker;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+/**
+ * CreateEditTaskDialog is a JDialog that handles creating and editing tasks.
+ * Depending on the constructor used, the JDialog shown will create or edit a task.
+ * This JDialog allows for a user to modify a Task's Name, Description, Status, Due Date,
+ * and color.
+ * Name and Description are inputted through JTextField and TTextArea respectively.
+ *
+ * Status is selected through a JComboBox, with the default selection being depending on whether
+ * a new Task is being created or if a task is being edited. If a new task is being created,
+ * the default status given is the one from which the Add Task button was pressed from.
+ *
+ * Due Date is selected through a JDatePicker. If a date is not selected, no due date is assigned.
+ *
+ * Color is selected through a JColorChooser Dialog. Default color is pink.
+ *
+ * Created by Lok Man Chu
+ */
 public class CreateEditTaskDialog extends JDialog {
     private JLabel nameLabel = new JLabel("Name: ");
     private JTextField nameInput = new JTextField(15);
@@ -25,16 +41,40 @@ public class CreateEditTaskDialog extends JDialog {
     private TaskModel taskModel;
     private TaskModel mainModel;
 
+    /**
+     * Static method for creating CreateEditTaskDialog.
+     * This call is used for editing a Task.
+     * @param parent MainScreen calling the Dialog
+     * @param model TaskModel to be edited
+     */
     public static void show(MainScreen parent, TaskModel model)
     {
         new CreateEditTaskDialog(parent, model, null);
     }
 
+    /**
+     * Static method for creating CreateEditTaskDialog.
+     * This call is used for creating a task
+     * @param parent MainScreen calling the Dialog
+     * @param status default status for new TaskModel
+     */
     public static void show(MainScreen parent, String status)
     {
         new CreateEditTaskDialog(parent,null, status);
     }
 
+    /**
+     * Private constructor of CreateEditTaskDialog. Use static CreateEditTaskDialog.show to create.
+     *
+     * Jobs covered:
+     * - Creates JDialog
+     * - Creates a TaskModel to make changes on.
+     * - Creates a Model for JComboBox
+     * - Calls GUI creation method
+     * @param parent
+     * @param model
+     * @param status
+     */
     private CreateEditTaskDialog(MainScreen parent, TaskModel model, String status) {
         super(SwingUtilities.getWindowAncestor(parent));
         this.parent = parent;
@@ -45,6 +85,16 @@ public class CreateEditTaskDialog extends JDialog {
         createGUI();
     }
 
+    /**
+     * Creates the GUI for CreateEditTaskDialog
+     *
+     * Jobs covered:
+     * - Sets Dialog settings for modality and resizing
+     * - Sets text to be for Creating Task or Editing Task
+     * - Sets default values of all input fields
+     * - Adds all elements to Dialog
+     * - Assigns ActionListeners to all buttons
+     */
     private void createGUI() {
 
         this.setModalityType(ModalityType.APPLICATION_MODAL);
@@ -55,6 +105,7 @@ public class CreateEditTaskDialog extends JDialog {
         confirmButton.setText(mainModel==null?"Create":"Edit");
         descriptionInput.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         descriptionInput.setLineWrap(true);
+        colorButton.setToolTipText("Click to change color");
 
 
         nameInput.setText(taskModel.getName());
@@ -110,15 +161,20 @@ public class CreateEditTaskDialog extends JDialog {
             this.dispose();
         });
 
-
-
         this.add(mainPanel);
         pack();
         setVisible(true);
-
-        System.out.println(descriptionInput.getSize());
     }
 
+    /**
+     * Method called when Create or Edit button is pressed.
+     *
+     * If Create, a new Task will be created with the values inputted into the Dialog's
+     * fields. Then it will add the new Task into the ProjectModel.
+     *
+     * If Edit, the given Task will have its values changed to the values inputted
+     * into the Dialog's field.
+     */
     private void confirm()
     {
         taskModel.setName(nameInput.getText());
